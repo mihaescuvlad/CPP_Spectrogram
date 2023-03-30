38c263d9e6c0d04faeb733753942f7dc7cc8e996
Mihaescu Vlad Mihai <mmihaivlad@yahoo.com>
2023-03-31 00:05:42 +0300

    Testing Lib
    Added sound files and tested the DLL linking

--- CPP_Spectogram/source/main.cpp
+++ CPP_Spectogram/source/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <vector>
 #include <sndfile.hh>
+
+const char* path = "input_data\\222677__unfa__ac-buzz-04.flac";
 
 int main()
 {
+    SNDFILE* file = nullptr;
+    SF_INFO info = {};
+
+    file = sf_open(path, SFM_READ, &info);
+    if (!file)
+    {
+        std::cerr << "Failed to open file " << path << std::endl;
+        return 1;
+    }
+
+    if (info.channels != 1)
+    {
+        std::cerr << "Only mono audio files are supported" << std::endl;
+        return 1;
+    }
+
+    std::vector<double> audio(info.frames);
+    sf_read_double(file, audio.data(), info.frames);
+
+    for (const auto& it : audio)
+    {
+        std::cout << it << ' ';
+    }
 
 	return 0;
 }
